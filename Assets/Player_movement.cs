da333185c664c39d83730f464432240b7bd30ff6
Søren Bjørn-Fallesen <falle1234@gmail.com>
2023-04-01 16:44:54 +0200

    nu med animator

--- Assets/Player_movement.cs
+++ Assets/Player_movement.cs
@@ -1,16 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Player_movement : MonoBehaviour
 {
-
-   [SerializeField] float speed = 10;
+    [SerializeField] Text scoreText;
+    [SerializeField] float speed = 10;
     SpriteRenderer sr;
     [SerializeField] float jumpPower = 200;
     Rigidbody2D rb;
     [SerializeField] LayerMask groundLayer;
     BoxCollider2D bc;
+    [SerializeField] int point;
 
     // Start is called before the first frame update
     void Start()
@@ -18,11 +19,13 @@ public class Player_movement : MonoBehaviour
         sr = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
         bc = GetComponent<BoxCollider2D>();
+        point = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //movement
         float dir = Input.GetAxisRaw("Horizontal");
         transform.Translate(dir * speed * Time.deltaTime, 0, 0);
         if (dir < 0)
@@ -32,11 +35,30 @@ public class Player_movement : MonoBehaviour
         bool jump = Input.GetButtonDown("Jump");
         if (jump && GroundCheck())
             rb.AddForce(Vector3.up * jumpPower);
-       
+
+        //score
+        scoreText.text = "Score: " + point;
+
     }
+    //ground Check
     bool GroundCheck()
     {
         return Physics2D.CapsuleCast(bc.bounds.center, bc.bounds.size,
             0f, 0f, Vector2.down, 0.5f, groundLayer);
+    }
+    //collision
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Trap
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            Destroy(gameObject);
+        }
+        //Frugt point
+        if (collision.gameObject.CompareTag("Fruits"))
+        {
+            point += 1;
+            Destroy(collision.gameObject);
+        }
     }
 }
